f5f1801a0d940223e8bfbd6db75248ca48edd740
Kevin Hinojosa <kevi123321@gmail.com>
2018-07-02 20:33:31 -0700

    first day push

---
+++ js/scripts.js
@@ -0,0 +1,21 @@
+var firstName;  //lets use camel casing
+
+
+//define variables
+firstName = "Ryeker"; //Strings
+
+//define and declare your viarable
+var lastName = "Herndon"
+
+console.log(firstName, lastName);
+
+firstName="Kevin"; //My new name is kevin
+
+console.log(firstName)
+
+var getTweetDataAsync;//bad variable name ex too ambigous. can be for live twitter data
+
+{
+  //new scope of var
+  var myName;
+}
