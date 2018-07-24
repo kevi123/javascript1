b07c54b3884222202127cd55fdf726199ad985dd
Kevin Hinojosa <kevi123321@gmail.com>
2018-07-23 20:58:21 -0700

    day7 end of day

--- js/scripts.js
+++ js/scripts.js
@@ -1,117 +1,61 @@
-/*
-Kevin Hinojosa Day 6 Notes
+//Day 7 more Dom Stuff
+//In this code we are modifying html via javascript. we add remove change classes and content
+const div = document.querySelector(".myDiv");   //in the Html file, we find the first tag that has class myDiv...this is what querySelector does, we then grab all the text in it and its children and put it in variable div
+const trekkies =" https://massolutions.biz/wp-content/uploads/2014/12/e5a06942fa42823c88be5f3a834e063d-fantastic-art-bat-family.jpg" // this puts our image into oue new variable
 
-*/
-
-// prompt("Hello enter value")
-// alert("ALert with this message")
-//
-//
-// if( x<0 || !(13)){
-//   statements;
-// } else-if(x!=50 && x<60 ){
-//   statements;
-// }else{
-//
-// };
-//
-// //Function SYntax
-//
-// let userAge2=0; //variable to hold age of user
-// userAge2=prompt("Lets Calculate your lifetime supply, what is your age?");
-//
-// let amount=0; //variable will hold user input of amount per year
-// amount=prompt("What is the amount per year?")
-// lifeTimeSupplyCalculator(userAge2, amount);
-//
-//
-// //this is the function for lifetimeSupply
-// function lifeTimeSupplyCalculator(age, productAmount){
-//   //Check for edge cases ie. negative age, alpha charactersS
-//   if (age<0){
-//     alert("EEROR: You cant be a negative age");
-//   }
-//   else if(80<age){
-//     alert("ERROr : Sorry you are too old!")
-//   }
-//   //this is the safe case,
-//   else{
-// // We need to check for chars
-//   let yearsValid=0;
-//   yearsValid= 80-age;
-//   lifeTimeSupply=yearsValid*productAmount;
-//   alert("The lifetime supply for this user is "+ lifeTimeSupply + "pounds!!")
-//   return(lifeTimeSupply)
-// }}
-
-/*
-
-//////////^^^^^^^^^^^^^^^^^^NOTES^^^^^^^^^^^^^
-
-const body = document.getElementsByTagName("body");  ///alays pass as a string so we dont pass an undeclared variable
-console.log(body[0]);
-
-// const header = document.getELementsByClassName("heading");
-
-
-//These are faster because it only searches for ID's
-const paragraph = document.getElementById("paragraph1");
-console.log("paragraph1");
-
-// let div = document.querySelector(".jr-is-the-best")
-// let div = document.querySelector("#beth_is_the_worst");
-
-// let divs = document.querySelectorAll(".jr-is-the-best");
-
-//we are listening for actions done by the mouse and keyboard
-div.addEventListener("mouseover", ev => {
-  console.log(ev);
-  div.style.backgroundColor = "#e40007";}
-);
+let image =documnet.querySelector("img");
 
 
 
-/*
-Write a program that converts temperatures. It should:
-
-    1)Ask your user to give you a scale (Celcius or Fahrenheit) and the tempurature they want to convert. So if they give you 90 Fahrenheit,
-      they would want you to convert 90 degrees Fahrenheit to whatever that is in Celcius
-    2)Employ a function that accepts those two values as arguments: temperature and scale (either celcius or fahrenheit) and
-      converts the tempurature they gave you to the opposite scale.
-    3)Display the temperature in an h3 with minimal styling.
-    4)From that point on, when the user clicks on the h3, run the function to convert the temperature back and forth between Celcius and Fahrenheit.
-    5)Display the converted temperature in the h3 each time it is changed.
-
-*/
-
-//We are saving this to an H3
-const h3= document.querySelector('#temp');
-let scale = prompt("WHat scale (C or F) do you want to use?", "F or C");
-let temp = prompt("How hot is it?", "99");  //this creates a prompt and prefills it with 99
 
 
-function convertTemp(scale, temp){
-  scale= scale.toLowerCase();
-  let returnVal;
-  let oppositeVal = scale === 'c' ? "Fahrenheit" : "Celcius";
-  temp = parseInt(temp);
-  if(!isNaN(temp)){
-    returnVal = scale === 'c' ? (1.8 * temp) + 32: (5/9) *(temp -32);  //this is known as a ternary statement
-    //this is read as if (scale==c){ returnVal= (1.8* temp)+32}  else {returnval=(5/9)*(temp-32)};
-    h3.innerText = `Your temperature is ${returnVal} ${oppositeVal}`;// we need to use back ticks to use placeholder variables.
-  }else{
-    scale = prompt("WHat scale (C or F) do you want to use?", "F or C");
-    temp = prompt("How hot is it", "99");   //this prompt asks a question and prefills it with 99
-    convertTemp(scale, temp);   //recursion call itself again
-  }
-}
-
-convertTemp(scale,temp);
+console.log(div.textContent);//Returns only the text in ALL children
+console.log(div);     //this lets us see every property of div in console
 
 
-h3.addEventListener('click',(e) =>{
-  let valueArray = e.target.innerText.split(' ');   //this splits innerText as an array [Your, temperature, is, returnVal, Calesius]
-    scale= valueArr[valueArr.length - 1 ]==="Celsius" ? "C": "F";
-    temp = valueArr[valueArr.length -2];
-    convertTemp(scale,temp)
+//div.textContent = "Say cheese and die!!"; //Reset the value of the text inside an element and replace it with this string
+//console.log(div.textContent);//Returns only the text in ALL children
+
+
+
+console.log(div.innerHTML);   //this grabs everthing in the first div, and returns text and tags
+
+div.innerHTML = "<h1>Hey there R.L stein, did you die in the 90s?</h1>" //we are replace all text and tags with this line
+
+
+let newH1 = document.createElement("h1");   //this created a varianle
+console.log(newH1)
+
+newH1.textcontext = "I am the greatest....I...I"; //this inserts the string into newH1
+
+div.appendChild(newH1);  //always adds AFter the last element in the parent
+
+let newH2 = document.createElement("h2");
+newH2.textContext = "I am before the h1"
+newH2.className ="blue-bg"; //this overwrites and adds this class to the tag
+newH2.className += "white-text"; /// this is a way to add additional classes
+console.log(newH2); //this logs every property of newH2 ie text, tags, characters
+div.insertBefore(newH2, newH1);
+
+let newP = document.createElement("p");   //we are creating a new p tag and saving the empty p tag in newP
+newP.textContent = "I am a squirrel...";  //we are adding this text content into this p tag
+newP.classList.add("purple-text", "green-bg");  //we are now adding these classes to our p tag
+div.appendChild(newP) // this is putting our ptag inside our div tag ie <div><p></p></div>
+
+
+let span = document.querySelector("span");    //lets search for the first instance of span and make a copy into variable span
+span.addEventListener("mouseenter", (e)=> {   //this is waiting for a mouse to click on span and will remove the white text color //mouseenter is a key word
+  span.classList.remove("white-text");    //this event is triggered when mouse enters
+});
+
+span.addEventListener("click", (e) =>{//we are waiting for user to click on the span
+  span.classList.toggle("giant-text");
+  span.classList.contains("green-bg");  //this looks at out span varible and checks if it contains class "green-bg". returns boolean
+  span.classList.replace("green-bg", "blue-bg");  // this will replace first parameter with second parameter
+})
+
+//attribute are like src= class= rel=
+
+image.addEventListener('click', (e) =>{
+  image.setAttribute("src", trekkies); // we are replacing the attribute of src to trekkies
 })
